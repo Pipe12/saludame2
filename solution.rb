259f0e09e2fe3276f46d050b5b97c27809192f92
Andrés Felipe Burbano <andresfelipeburbano@hotmail.com>
2017-06-08 19:29:44 -0500

    dele solo el uso de rutas

--- solution.rb
+++ solution.rb
@@ -1,13 +1,7 @@
 require 'sinatra'
 
 get '/makers/:nombre' do
-  unless params[:nombre]
-      <<-HTML
-        <h1>"Hola desconocido!"</h1>
-      HTML
-    else
       <<-HTML
         <h1>"Hola #{params[:nombre].capitalize}!"</h1>
       HTML
-  end
 end
